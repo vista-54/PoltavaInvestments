cd664c79b22c0c2faad0c9b4ea93f05d5be98178
vista-54 <vista-54@ukr.net>
2016-05-23 19:32:12 +0300

    added: swipe

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,17 +1,26 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
         "clobbers": [
-            "navigator.splashscreen"
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-splashscreen": "4.0.0",
+    "cordova-plugin-console": "1.0.4-dev",
+    "cordova-plugin-network-information": "1.2.2-dev",
     "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
